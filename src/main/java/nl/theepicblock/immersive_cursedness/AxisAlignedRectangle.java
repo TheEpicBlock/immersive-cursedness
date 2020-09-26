3b69e5d6e3d990506326e5b2451aa4a83d28523e
TheEpicBlock <61842090+TheEpicBlock@users.noreply.github.com>
2020-09-26 22:49:39 +0200

    Eh, dunno what I'm trying to do here

--- src/main/java/nl/theepicblock/immersive_cursedness/AxisAlignedRectangle.java
+++ src/main/java/nl/theepicblock/immersive_cursedness/AxisAlignedRectangle.java
@@ -3,59 +3,12 @@ package nl.theepicblock.immersive_cursedness;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
 
-public class AxisAlignedRectangle {
-    //Right is defined as the most positive point in whatever axis this is
-    private BlockPos upperRight;
-    private BlockPos lowerLeft;
-    private Direction.Axis axis;
+public interface AxisAlignedRectangle<T> {
+    double getDistance(T pos);
 
-    public AxisAlignedRectangle(BlockPos upperRight, BlockPos lowerLeft, Direction.Axis axis) {
-        this.upperRight = upperRight;
-        this.lowerLeft = lowerLeft;
-        this.axis = axis;
-    }
+    T getUpperRight();
 
-    public double getDistance(BlockPos pos) {
-        return upperRight.getSquaredDistance(pos);
-    }
+    T getLowerLeft();
 
-    /**
-     * Returns true if this rectangle fully encloses b
-     */
-    public boolean contains(AxisAlignedRectangle b) {
-        if (this.getAxis() != b.getAxis()) return false;
-        if (this.getTop() < b.getTop() ||
-            this.getBottom() > b.getBottom()) return false;
-        Direction.Axis axis = this.getAxis();
-        return this.getRight() >= b.getRight() &&
-               this.getLeft() <= b.getLeft();
-    }
-
-    public BlockPos getUpperRight() {
-        return upperRight;
-    }
-
-    public BlockPos getLowerLeft() {
-        return lowerLeft;
-    }
-
-    public Direction.Axis getAxis() {
-        return axis;
-    }
-
-    public int getLeft() {
-        return Util.get(this.getLowerLeft(), this.getAxis());
-    }
-
-    public int getRight() {
-        return Util.get(this.getUpperRight(), this.getAxis());
-    }
-
-    public int getTop() {
-        return this.getUpperRight().getY();
-    }
-
-    public int getBottom() {
-        return this.getLowerLeft().getY();
-    }
+    Direction.Axis getAxis();
 }
--- src/main/java/nl/theepicblock/immersive_cursedness/Portal.java
+++ src/main/java/nl/theepicblock/immersive_cursedness/Portal.java
@@ -3,8 +3,65 @@ package nl.theepicblock.immersive_cursedness;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
 
-public class Portal extends AxisAlignedRectangle {
+import javax.sound.sampled.Port;
+
+public class Portal implements AxisAlignedRectangle<BlockPos> {
+    //Right is defined as the most positive point in whatever axis this is
+    private BlockPos upperRight;
+    private BlockPos lowerLeft;
+    private Direction.Axis axis;
+
     public Portal(BlockPos upperRight, BlockPos lowerLeft, Direction.Axis axis) {
-        super(upperRight, lowerLeft, axis);
+        this.upperRight = upperRight;
+        this.lowerLeft = lowerLeft;
+        this.axis = axis;
+    }
+
+    @Override
+    public double getDistance(BlockPos pos) {
+        return upperRight.getSquaredDistance(pos);
+    }
+
+    /**
+     * Returns true if this rectangle fully encloses b
+     */
+    public boolean contains(Portal b) {
+        if (this.getAxis() != b.getAxis()) return false;
+        if (this.getTop() < b.getTop() ||
+                this.getBottom() > b.getBottom()) return false;
+        Direction.Axis axis = this.getAxis();
+        return this.getRight() >= b.getRight() &&
+                this.getLeft() <= b.getLeft();
+    }
+
+    @Override
+    public BlockPos getUpperRight() {
+        return upperRight;
+    }
+
+    @Override
+    public BlockPos getLowerLeft() {
+        return lowerLeft;
+    }
+
+    @Override
+    public Direction.Axis getAxis() {
+        return axis;
+    }
+
+    public int getLeft() {
+        return Util.get(this.getLowerLeft(), this.getAxis());
+    }
+
+    public int getRight() {
+        return Util.get(this.getUpperRight(), this.getAxis());
+    }
+
+    public int getTop() {
+        return this.getUpperRight().getY();
+    }
+
+    public int getBottom() {
+        return this.getLowerLeft().getY();
     }
 }
