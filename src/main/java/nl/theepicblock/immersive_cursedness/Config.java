5e26cc8cf3318ad5d537d84a40b3eeff48b64280
TheEpicBlock <61842090+TheEpicBlock@users.noreply.github.com>
2020-11-08 18:03:20 +0100

    Fix Config

--- src/main/java/nl/theepicblock/immersive_cursedness/Config.java
+++ src/main/java/nl/theepicblock/immersive_cursedness/Config.java
@@ -7,26 +7,25 @@ import me.sargunvohra.mcmods.autoconfig1u.shadowed.blue.endless.jankson.Comment;
 @me.sargunvohra.mcmods.autoconfig1u.annotation.Config(name = "immersive-cursedness")
 public class Config implements ConfigData {
 	public int horizontalSendLimit = 70;
+	@Comment("Should usually be atmosphereRadius+2")
 	public int portalDepth = 30;
 	@Comment("The radius where the outer block of the atmosphere should be")
 	private int atmosphereRadius = 28;
 	@Comment("Measured in chunks")
 	public int renderDistance = 3;
 
-	final public transient double squaredAtmosphereRadius;
-	final public transient double squaredAtmosphereRadiusPlusOne;
-	final public transient double squaredAtmosphereRadiusMinusOne;
-
-	public Config() {
-		squaredAtmosphereRadius = Math.pow(atmosphereRadius, 2);
-		squaredAtmosphereRadiusPlusOne = Math.pow(atmosphereRadius+1, 2);
-		squaredAtmosphereRadiusMinusOne = Math.pow(atmosphereRadius-1, 2);
-	}
+	public transient double squaredAtmosphereRadius;
+	public transient double squaredAtmosphereRadiusPlusOne;
+	public transient double squaredAtmosphereRadiusMinusOne;
 
 	@Override
 	public void validatePostLoad() throws ValidationException {
 		if (atmosphereRadius >= portalDepth) {
 			throw new ValidationException("atmosphereRadius should be smaller then portalDepth");
 		}
+
+		squaredAtmosphereRadius = Math.pow(atmosphereRadius, 2);
+		squaredAtmosphereRadiusPlusOne = Math.pow(atmosphereRadius+1, 2);
+		squaredAtmosphereRadiusMinusOne = Math.pow(atmosphereRadius-1, 2);
 	}
 }
