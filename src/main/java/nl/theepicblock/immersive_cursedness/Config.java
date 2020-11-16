8ce5da803975e40540199efd5c388cf522ed081b
TheEpicBlock <61842090+TheEpicBlock@users.noreply.github.com>
2020-11-16 20:55:15 +0100

    Edit maxPacketsPerClientPerTick default

--- src/main/java/nl/theepicblock/immersive_cursedness/Config.java
+++ src/main/java/nl/theepicblock/immersive_cursedness/Config.java
@@ -14,7 +14,7 @@ public class Config implements ConfigData {
 	@Comment("Measured in chunks")
 	public int renderDistance = 3;
 	@Comment("Increasing will make the portal load faster. But will make the portal lag behind when moving. It's recommended to leave on default.")
-	public int maxPacketsPerClientPerTick = 100;
+	public int maxPacketsPerClientPerTick = 250;
 	public boolean debugParticles = false;
 
 	public transient double squaredAtmosphereRadius;
